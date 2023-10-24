52d74c231a7310e0440d5434bedf8946a029925d
walid-yahia-mahammed <w.yahia-mahammed@univ-dbkm.dz>
2023-10-24 11:49:22 +0100

    integrate herp component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+// import FeaturedPosts from "../components/home-page/featured-posts";
 function HomePage() {
+  return <Fragment>
+    <Hero />
+  </Fragment>
 }
 
 export default HomePage;
